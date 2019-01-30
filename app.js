7a15fd27e6e61930305fb1d6dcacdc552dc08676
Edinyanga O. Ottoho <elzucky@gmail.com>
2019-01-30 22:40:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 var t = "";
+var p = "";
+var a = "";
+var total = 0;
 function pay(){
-    var xp = t.split("||");
-	  var pr = parseInt(xp[2]);
-	  var amount = pr * 100;
-	  var email = xp[1];
-	  var product = t[0];
-      var handler = PaystackPop.setup({
-         key:'pk_test_ccb78c39f39b80d0b47734286a4a2d1d9dcc3ab1',
-         email:email,
-         amount:amount,
-         metadata:{
-         	custom_fields:[
-         	  {
-         	  	display_name:"Product",
-         	  	variable_name:"product_name",
-         	  	value:product
-         	  }
-         	]
-         },
-         callback:function(response){
-           alert("Transaction Complete");
-         },
-         onClose:function(){
-           alert("Transaction Cancelled");
-         }
-      });
-      handler.openIframe();
+    var spl = t.split("||");
+    if (p == "" && a == ""){
+      p += spl[0];
+      a += spl[1];
+    }
+    else{
+      p += "," + spl[0];
+      a += "," + spl[1];
+    }
+}
+function cl(){
+  document.getElementById("cart").style.visibility='hidden';
+}
+function cart(){
+   document.getElementById("cart").style.visibility='visible';
+   var docx = document.getElementById("cartlist");
+   if (p != ""){
+      var pr = p.split(",");
+      var am = a.split(",");
+      total = 0;
+      docx.innerHTML = "";
+      for (var cnt=0;cnt<=am.length-1;cnt++){
+        docx.innerHTML += '<tr><td class="item"><div>'+ pr[cnt] +'</div></td><td class="price"><font color="black">NGN</font> ' + am[cnt] +'</div></td></tr>';
+        total = total + parseInt(am[cnt]);
+      }
+      docx.innerHTML += '<tr><td class="total"><div>Total</div></td><td class="totalamount"><font color="black">NGN</font> '+ total +'</div></td></tr>';
+      docx.innerHTML += '<tr><td colspan="2" style="padding-top:40px"><center><button class="checkout" onclick="paywithpaystack()">Checkout</button></center></td></tr>';
+   }
+   else{
+      docx.innerHTML = '<tr><td class="item"><center>Nothing available yet</center></td></tr>';
+   }
 }
 function notify(){
 	var interv = setTimeout(function(){
@@ -39,7 +47,7 @@ function notify(){
 		}
 		else if (Notification.permission !== "denied"){
             Notification.requestPermission(function(permission){
-              if (!('permission' in windows)){
+              if (!('permission' in window)){
                  Notification.permission = permission;
               }
               if (permission === "granted"){
